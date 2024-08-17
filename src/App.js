97979e837bd658d26e75b77f4a1d5d8d246c1154
ONE LEASING <oneleasing1@gmail.com>
2024-08-17 11:02:54 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Layout from "./ui/component/main/Layout";
+import SignIn from "./pages/SignIn";
 
 function App() {
+  const token = false;
+
+  if (!token) {
+    return (
+      <>
+        <SignIn />
+      </>
+    );
+  }
   return (
     <>
       <Layout />
