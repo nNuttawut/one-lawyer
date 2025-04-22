46fc3c088ddae05242f64539f0f64214a3b6b7c4
ONE LEASING <oneleasing1@gmail.com>
2025-04-22 15:54:37 +0700

    remove build directory from tracking

--- build/static/js/952.3f8d4d1e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklawyer_app=self.webpackChunklawyer_app||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.3f8d4d1e.chunk.js.map
